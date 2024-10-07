670dd6a344ea423a8f97a0d9c43f408e8dceea9d
Luccas Gabriel <luccasgpereira720@gmail.com>
2024-10-07 01:23:58 -0300

    Data de nascimento
    O código pede o dia,mês e o ano do nascimento do úsuario, depois informa na tela ("dd/mm/aa")

---
+++ data de nascimento.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia a data de nascimento
+#  do usuario e mostra uma mensagem com a idade informada 
+
+dia = input ('Informe o dia do seu nascimento:')
+mes = input ('Informe o mês do seu nascimento:')
+ano = input ('Informe o ano do seu nascimento?:')
+print('Você nasceu:',dia,'/',mes,'/',ano)
