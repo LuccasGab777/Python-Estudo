5914807604084e256e5d4529e91b9661a7912004
Luccas Gabriel <luccasgpereira720@gmail.com>
2024-10-07 01:42:05 -0300

    Soma de dois números
    O programa  pede a soma de dois números, onde o código usa a classe 'float' que representa um número decimal. Depois disso ele exibe a soma dos valores.

---
+++ Soma de dois números.py
@@ -0,0 +1,10 @@
+#Crie um programa que soma dois números informado pelo úsuario
+
+from tokenize import Double
+
+
+print("Informe dois valores para somar")
+soma1 = float (input ("Primeiro valor:"))
+soma2 = float (input ("Segundo valor:"))
+somaTotal = soma1+soma2
+print(somaTotal)
