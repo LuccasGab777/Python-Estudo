c0e2802faf24717bef8c6d3cd9f894fbc79cd1d0
Luccas Gabriel <luccasgpereira720@gmail.com>
2024-10-07 03:21:15 -0300

    Update Ex1-Chamar nome.py

--- Ex1-Chamar nome.py
+++ Ex1-Chamar nome.py
@@ -1,3 +1,8 @@
 print("Crie um script phthon que leia o nome de uma pessoa e mostre uma mensagemde boas-vindas de acordo com o valor digitado")
 nome = input ('Informe o seu nome ?')
 print("Olá ",nome," seja bem vindo")
+
+#Para que o usuario informe um valor em python, eu preciso declarar um objeto no meu caso 'nome' como 'input' e inserir um texto
+#O 'input' irá imprimir a mensagem digitalizada na tela ex input('Insira um valor') e logo em seguida irá pedir um valor ao usúario
+#O 'print' apenas exibe a mensagem, e eu posso colocar a minha variavél nome dentro do texto do 'print',
+#para que ele exiba o valor informado pelo o usuario
