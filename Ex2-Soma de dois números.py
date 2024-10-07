9ac25d299e93fcf0f699d4c4512fe78dbb35b665
Luccas Gabriel <luccasgpereira720@gmail.com>
2024-10-07 03:14:35 -0300

    Update Ex2-Soma de dois números.py

--- Ex2-Soma de dois números.py
+++ Ex2-Soma de dois números.py
@@ -8,3 +8,8 @@ soma1 = float (input ("Primeiro valor:"))
 soma2 = float (input ("Segundo valor:"))
 somaTotal = soma1+soma2
 print(somaTotal)
+
+#Eu usei 'float' para declarar o número informado pelo usuario como um número decimal
+#Pois se eu não usar 'float' ou 'int', o valor informado pelo usuario irá se juntar
+#Número informado pelo usuario sem declarar 'float' ou 'int'. ex: 2+2=22
+#Número informado pelo usuario usando 'float' ou 'int'. ex: 2+2=4
