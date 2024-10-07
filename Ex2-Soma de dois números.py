926581023ca9f9794dcea3bfc6f89c0b1e958f35
Luccas Gabriel <luccasgpereira720@gmail.com>
2024-10-07 03:37:12 -0300

    Update Ex2-Soma de dois números.py

--- Ex2-Soma de dois números.py
+++ Ex2-Soma de dois números.py
@@ -1,13 +1,11 @@
 #Crie um programa que soma dois números informado pelo úsuario
 
-from tokenize import Double
-
 
 print("Informe dois valores para somar")
 soma1 = float (input ("Primeiro valor:"))
 soma2 = float (input ("Segundo valor:"))
 somaTotal = soma1+soma2
-print(somaTotal)
+print('A soma entre',soma1,' e ',soma2,' é: ',somaTotal)
 
 #Eu usei 'float' para declarar o número informado pelo usuario como um número decimal
 #Pois se eu não usar 'float' ou 'int', o valor informado pelo usuario irá se juntar
