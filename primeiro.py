6dcf188b52179ce4582d231a1b7875ad79546e17
Luccas Gabriel <luccasgpereira720@gmail.com>
2024-10-07 01:09:19 -0300

    chamar nome
    Meu primeiro exercicio em Python
    Objetivo: Pedir o nome do usúario e exibir na tela "Olá (nome do usúario), seja bem vindo!"

---
+++ primeiro.py
@@ -0,0 +1,3 @@
+print("Crie um script phthon que leia o nome de uma pessoa e mostre uma mensagemde boas-vindas de acordo com o valor digitado")
+nome = input ('Informe o seu nome ?')
+print("Olá ",nome," seja bem vindo")
